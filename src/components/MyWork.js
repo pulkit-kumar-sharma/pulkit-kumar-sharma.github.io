f1a39018300439bff0c3f9a54f9cf6b8c7e76680
Sharma, Pulkit K <pulkits1998@gmail.com>
2022-08-22 15:25:25 +0530

    Added Work

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { GrPrevious, GrNext } from 'react-icons/gr';
 import { AiFillGithub } from 'react-icons/ai';
+import { BsLaptop } from 'react-icons/bs';
 
 function MyWork() {
   return (
@@ -63,21 +64,21 @@ function MyWork() {
                         </div>
                         
                     </div>
-                    {/* <div className="col-md-4 text-center my-2">
+                    <div className="col-md-4 text-center my-2">
                         
                         <div id="carouselExampleControls3" className="carousel slide my-1" data-bs-ride="carousel">
                             <div className="carousel-inner">
                                 <div className="carousel-item active">
-                                <img src="../portfolio-images/n-01.png" className="d-block w-100" alt="..."/>
+                                <img src="../portfolio-images/LoveCalculator-1.png" className="d-block w-100" alt="..."/>
                                 </div>
                                 <div className="carousel-item">
-                                <img src="../portfolio-images/n-02.png" className="d-block w-100" alt="..."/>
+                                <img src="../portfolio-images/LoveCalculator-2.png" className="d-block w-100" alt="..."/>
                                 </div>
                                 <div className="carousel-item">
-                                <img src="../portfolio-images/n-03.png" className="d-block w-100" alt="..."/>
+                                <img src="../portfolio-images/LoveCalculator-3.png" className="d-block w-100" alt="..."/>
                                 </div>
                                 <div className="carousel-item">
-                                <img src="../portfolio-images/n-04.png" className="d-block w-100" alt="..."/>
+                                <img src="../portfolio-images/LoveCalculator-4.png" className="d-block w-100" alt="..."/>
                                 </div>
                             </div>
                             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls3" data-bs-slide="prev">
@@ -89,44 +90,13 @@ function MyWork() {
                                 <span className="visually-hidden">Next</span>
                             </button>
                         </div>
-                            <h5 className="ff-jose my-1">Notes MarketPlace</h5>
+                            <h5 className="ff-jose my-1">Love Calculator</h5>
                             <div className="row d-flex justify-content-center">
-                                <a href="https://github.com/Yagnik-Gohil/NoteMarketPlaceHTML" className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded" target="/blank"><AiFillGithub/> Source Code</a>
+                                <a href="https://pulkit-kumar-sharma.github.io/LoveCalculator/" className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded" target="/blank"><BsLaptop/> Watch Live</a>
+                                <a href="https://github.com/pulkit-kumar-sharma/LoveCalculator/" className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded" target="/blank"><AiFillGithub/> Source Code</a>
                             </div>
                         
                     </div>
-                    <div className="col-md-4 text-center my-2">
-                        
-                        <div id="carouselExampleControls4" className="carousel slide my-1" data-bs-ride="carousel">
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                <img src="../portfolio-images/solo-01.png" className="d-block w-100" alt="..."/>
-                                </div>
-                                <div className="carousel-item">
-                                <img src="../portfolio-images/solo-02.png" className="d-block w-100" alt="..."/>
-                                </div>
-                                <div className="carousel-item">
-                                <img src="../portfolio-images/solo-03.png" className="d-block w-100" alt="..."/>
-                                </div>
-                                <div className="carousel-item">
-                                <img src="../portfolio-images/solo-04.png" className="d-block w-100" alt="..."/>
-                                </div>
-                            </div>
-                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls4" data-bs-slide="prev">
-                                <GrPrevious/>
-                                <span className="visually-hidden">Previous</span>
-                            </button>
-                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls4" data-bs-slide="next">
-                                <GrNext/>
-                                <span className="visually-hidden">Next</span>
-                            </button>
-                        </div>
-                            <h5 className="ff-jose my-1">Solo</h5>
-                            <div className="row d-flex justify-content-center">
-                                <a href="https://github.com/Yagnik-Gohil/Solo" className="col-sm-12 col-md-5 portfolio-link text-decoration-none text-white m-1 px-2 py-1 bg-blue rounded" target="/blank"><AiFillGithub/> Source Code</a>
-                            </div>
-                        
-                    </div> */}
                 </div>
                 
             </div>
